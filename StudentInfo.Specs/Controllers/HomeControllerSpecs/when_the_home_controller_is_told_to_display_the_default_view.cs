2fbe15d55db1a5ff9ca5bac9479d1400edac5fa9
Jboy Flaga <jeremiahflaga@gmail.com>
2014-06-11 16:28:33 +0800

    Added StudentInfo.Domain porject; modified test code

--- StudentInfo.Specs/Controllers/HomeControllerSpecs/when_the_home_controller_is_told_to_display_the_default_view.cs
+++ StudentInfo.Specs/Controllers/HomeControllerSpecs/when_the_home_controller_is_told_to_display_the_default_view.cs
@@ -12,31 +12,31 @@ namespace Specs.Controllers.HomeControllerSpecs
     [TestClass]
     public class when_the_home_controller_is_told_to_display_the_default_view
     {
-        HomeController m_controller;
-        ViewResult m_viewResult;
+        HomeController _controller;
+        ViewResult _viewResult;
 
         [TestInitialize]
         public void Setup()
         {
-            // arrange
-            m_controller = new HomeController();
+            // given
+            _controller = new HomeController();
 
-            // act
-            m_viewResult = m_controller.Index() as ViewResult;
+            // when
+            _viewResult = _controller.Index() as ViewResult;
         }
 
         [TestMethod]
         public void it_should_return_the_home_view()
         {
-            // assert
-            Assert.IsTrue(string.IsNullOrEmpty(m_viewResult.ViewName));
+            // then
+            Assert.IsTrue(string.IsNullOrEmpty(_viewResult.ViewName));
         }
 
         [TestMethod]
         public void it_should_display_the_welcome_message_in_the_view()
         {
-            // assert
-            Assert.AreEqual("Welcome!", m_controller.ViewBag.Message);
+            // then
+            Assert.AreEqual("Welcome!", _controller.ViewBag.Message);
         }
     }
 }
